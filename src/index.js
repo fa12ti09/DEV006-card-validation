adf5c8ebf49d3c07194f4e02b6e8a4d795e1b877
Fatima <fatima_gtz@outlook.com>
2023-03-30 21:03:29 -0600

    commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import validator from './validator.js';
+import validator from './validator.js'
 // numero tarjeta
-const numeroTarjeta = document.getElementById("inputNumero");
+const numeroTarjeta = document.getElementById('inputNumero')
 const btnSubmit = document.getElementById('btnsubmit')
 
-btnSubmit.addEventListener("click", function (event) {
-event.preventDefault()
-    
-console.log("Tarjeta Válida");
-console.log(validator.isValid(numeroTarjeta.value));
-alert("Gracias por Donar");
-const maskedNumber = validator.maskify(numeroTarjeta.value);
-console.log(maskedNumber); // Output: "#### #### #### 7454"
+btnSubmit.addEventListener('click', function (event) {
+  event.preventDefault()
 
+  //console.log('Validando Tarjeta')
+  const luhn = validator.isValid(numeroTarjeta.value)
+  const maskify = validator.maskify(numeroTarjeta.value)
+  //alert(maskify) // Output: "#### #### #### 7454"
+  //alert('Gracias por Donar');
+  if (luhn === true) {
+    alert('Tarjeta ' + maskify + 'Es Válida')
+    alert('MUCHAS GRACIAS POR TU DONACIÓN')
+  } else {
+    alert('Tarjeta ' + maskify + ' Es Inválida')
+    alert(' ERROR: Intente con otra tarjeta')
+  }
 })
 
-
-
-
-
-    
-
-
-
+//console.log(validator)
--- src/validator.js
+++ src/validator.js
@@ -1,64 +1,45 @@
 const validator = {
-  isValid:function(numeroTarjeta){ 
-    //codigo de validar
-   
+  isValid: function (numeroTarjeta) {
     //  Convertir el número de la tarjeta a un array de dígitos
-    const digitos = numeroTarjeta.split("").map(Number);
-    
+    const digitos = numeroTarjeta.split('').map(Number)
+
     //  Iterar sobre los dígitos, comenzando desde el penúltimo dígito y retrocediendo
     for (let i = digitos.length - 2; i >= 0; i -= 2) {
-//  Multiplicar cada segundo dígito por 2
-digitos[i] *= 2;
-    
-//  Si el resultado de la multiplicación es mayor o igual a 10, sumar los dígitos individuales
-if (digitos[i] >= 10) {
-digitos[i] = digitos[i] % 10 + Math.floor(digitos[i] / 10);
-}
-}
-    
-//  Sumar todos los dígitos
-const sumaDigitos = digitos.reduce((total, digit) => total + digit, 0);
-    
-//  La tarjeta es válida si la suma de los dígitos es divisible por 10
-if sumaDigitos % 10 === 0;
-  
-},
-maskify:function(numeroTarjeta) {
-// Obtener la longitud de la cadena de la tarjeta
-const cardLength = numeroTarjeta.length;
-      
-// Si la longitud de la cadena de la tarjeta es menor o igual a 4, devolver la cadena original
- if (cardLength <= 4) {
-return numeroTarjeta;
-} else{  
-// Reemplazar todos los caracteres excepto los últimos 4 con "#"
-let mask = "#";
-let maskedNumber = "";
-// Contador for inicia en 0 hasta la posición 12
-for (let i = 0; i < cardLength - 4; i++) {
-maskedNumber = (maskedNumber[i]);
-//console.log(maskedNumber);
-maskedNumber + mask;
+      //  Multiplicar cada segundo dígito por 2
+      digitos[i] *= 2
 
-//console.log(maskedNumber);
-//Devolver el número de tarjeta enmascarado
-return maskedNumber;
-}
-//return maskedNumber + numeroTarjeta.substring(cardLength - 4); 
-console.log(maskedNumber);
-}
+      //  Si el resultado de la multiplicación es mayor o igual a 10, sumar los dígitos individuales
+      if (digitos[i] >= 10) {
+        digitos[i] = (digitos[i] % 10) + Math.floor(digitos[i] / 10)
+      }
+    }
+
+    //Sumar todos los dígitos
+    const sumaDigitos = digitos.reduce((total, digit) => total + digit, 0)
+
+    //  La tarjeta es válida si la suma de los dígitos es divisible por 10
+    if (sumaDigitos % 10 === 0) {
+      //si el resto de 10 es igual a 0
+      // alert('Tu tarjeta es Válida')
+      return true
+    } else {
+      //alert('Error: Número de tarjeta invalido')
+      return false
+    }
+  },
+  maskify: function (numeroTarjeta) {
+    // crea un nuevo array
+    const maskedNumber = Array.from(numeroTarjeta)
+    // bucle
+    for (let i = 0; i <= numeroTarjeta.length; i++) {
+      // se enmascara el numero de la posicion 0 al 12
+      if (i < maskedNumber.length - 4) {
+        maskedNumber[i] = '#'
+      }
+    }
+    // devuelve el numero enmascarado
+    return maskedNumber.join('')
+  }
 }
 
-}
-  
-export default validator;
-
-
-
-
-
-
-
-
-
-
+export default validator
